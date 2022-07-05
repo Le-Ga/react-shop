603fe697ae8712a78db1df075ff0f7493ba1097d
Олег Малютин <91494915+Le-Ga@users.noreply.github.com>
2022-07-05 12:51:00 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
               <a
-                href="https://github.com/Le-Ga/react-movies"
+                href="https://github.com/Le-Ga/react-shop"
                 target="_blank"
                 rel="noreferrer"
               >
